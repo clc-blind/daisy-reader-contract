6693f4b1896aabda9903264bea0bac4765b7f139
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2025-11-11 08:13:46 +0700

    fix(openapi): add better-auth routes

--- src/libs/openApi.ts
+++ src/libs/openApi.ts
@@ -1,10 +1,33 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { generateOpenApi } from '@ts-rest/open-api';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { betterAuth } from 'better-auth';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { openAPI } from 'better-auth/plugins';
 import { appContract } from '@/src/contract';
 
-export const openApiDocument = generateOpenApi(appContract, {
-  info: {
-    title: 'DAISY API',
-    version: '1.0.0',
-  },
-});
+const generateOpenApiDocument = async () => {
+  const baseOpenApiDocument = generateOpenApi(appContract, {
+    info: {
+      title: 'DAISY API',
+      version: '1.0.0',
+    },
+  });
+
+  const betterAuthOpenApiDocument = await betterAuth({
+    emailAndPassword: {
+      enabled: true,
+    },
+    plugins: [openAPI()],
+  }).api.generateOpenAPISchema();
+
+  return {
+    ...baseOpenApiDocument,
+    paths: {
+      ...baseOpenApiDocument.paths,
+      ...betterAuthOpenApiDocument.paths,
+    },
+  };
+};
+
+export const openApiDocument = await generateOpenApiDocument();
